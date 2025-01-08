0f86294513cdf73a50eac8d716a1e58bf7b883bf
andrey <i78901234567890@gmail.com>
2025-01-08 21:32:20 +0300

    v5

--- bot_logic.py
+++ bot_logic.py
@@ -13,6 +13,56 @@ with open(os.path.join(DIR_PATH, 'token.txt'), 'r') as token:
     TOKEN = token.read().strip()
 
 
+def is_jellifyn_running() -> bool:
+    try:
+        # Проверка статуса процесса через systemctl
+        subprocess.run(['systemctl', 'is-active', '--quiet', 'jellyfin'], check=True)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+
+async def check_jellyfin_status(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if is_jellifyn_running():
+        message = "Jellyfin запущен"
+    else:
+        message = "Jellyfin остановлен"
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+
+async def get_jellyfin_logs(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Получение последних 15 строк логов из systemd
+    logs = subprocess.check_output(['journalctl', '-u', 'jellyfin', '--lines', '15'], stderr=subprocess.STDOUT)
+    message = f"Логи:\n {logs.decode('utf-8')}"
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+
+async def get_jellyfin_errors(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Получаем последние 15 ошибок (предполагаем, что они содержат слово "error")
+    logs = subprocess.check_output(
+        ['journalctl', '-u', 'jellyfin', '--lines', '15', '--grep', 'error'], stderr=subprocess.STDOUT
+    )
+    message = f"Логи:\n {logs.decode('utf-8')}"
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+
+async def restart_jellyfin(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    try:
+        # Перезапуск сервиса Jellyfin
+        subprocess.run(['sudo', 'systemctl', 'restart', 'jellyfin'], check=True)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Jellyfin перезапущен")
+    except subprocess.CalledProcessError as e:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Ошибка перезапуска Jellyfin: {e}")
+
+
+async def top_statistics(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    top_output = subprocess.check_output(['top', '-n', '1', '-o', '-%CPU', '-b'], stderr=subprocess.STDOUT).decode(
+        'utf-8'
+    )
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"<pre>{top_output}</pre>", parse_mode='HTML')
+
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
@@ -40,7 +90,7 @@ async def get_system_status():
         try:
             if partition.mountpoint in allowed_mount_points or allowed_devices.match(partition.device.split("/")[-1]):
                 usage = psutil.disk_usage(partition.mountpoint)
-                total_size = usage.total / (1024 ** 3)  # В гигабайтах
+                total_size = usage.total / (1024**3)  # В гигабайтах
                 disk_info.append(
                     f"  🔹 <b>Диск {partition.device}</b> ({partition.mountpoint}):"
                     f"\n  📊 <b>{usage.percent}%</b> использовано"
@@ -80,6 +130,11 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "/status - Показать статус системы (температура, загрузка ЦП и ОЗУ)\n"
         "/check_ssd_2tb - Проверяет - доступен ли SSD для чтения\n"
         "/remount_ssd_2tb - Заново перемонтирует SSD\n"
+        "/check_jellyfin_status - Статус jellyfin\n"
+        "/get_jellyfin_logs - Лог jellyfin\n"
+        "/restart_jellyfin - Перезапуск jellyfin\n"
+        "/get_jellyfin_errors - Лог ошибок jellyfin\n"
+        "/top_statistics - Статистика процессов\n"
     )
     await context.bot.send_message(chat_id=update.effective_chat.id, text=help_text)
 
@@ -117,11 +172,21 @@ if __name__ == '__main__':
     help_handler = CommandHandler('help', help)
     check_ssd_handler = CommandHandler('check_ssd_2tb', status_ssd_2tb)
     remount_ssd_handler = CommandHandler('remount_ssd_2tb', remount_ssd_2tb)
+    check_jellyfin_status_handler = CommandHandler('check_jellyfin_status', check_jellyfin_status)
+    get_jellyfin_logs_handler = CommandHandler('get_jellyfin_logs', get_jellyfin_logs)
+    restart_jellyfin_handler = CommandHandler('restart_jellyfin', restart_jellyfin)
+    get_jellyfin_errors_handler = CommandHandler('get_jellyfin_errors', get_jellyfin_errors)
+    top_statistics_handler = CommandHandler('top_statistics', top_statistics)
 
     application.add_handler(start_handler)
     application.add_handler(status_handler)
     application.add_handler(help_handler)
     application.add_handler(check_ssd_handler)
     application.add_handler(remount_ssd_handler)
+    application.add_handler(check_jellyfin_status_handler)
+    application.add_handler(get_jellyfin_logs_handler)
+    application.add_handler(restart_jellyfin_handler)
+    application.add_handler(get_jellyfin_errors_handler)
+    application.add_handler(top_statistics_handler)
 
     application.run_polling()
