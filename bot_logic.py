3b18306978b1ef61b3edc2248b575dbaf01ab240
andrey <i78901234567890@gmail.com>
2025-01-08 19:10:28 +0300

    v2

--- bot_logic.py
+++ bot_logic.py
@@ -40,8 +40,34 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "/start - Начать работу с ботом\n"
         "/help - Показать список доступных команд\n"
         "/status - Показать статус системы (температура, загрузка ЦП и ОЗУ)\n"
+        "/check_ssd_2tb - Проверяет - доступен ли SSD для чтения\n"
     )
     await context.bot.send_message(chat_id=update.effective_chat.id, text=help_text)
+
+
+async def status_ssd_2tb(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Функция проверяет статус SSD на 2 ГБ."""
+    directory = "/mnt/SSD_4TB/D"
+    try:
+        # если диск размонтировался, то выпадет ошибка чтения
+        os.listdir(directory)
+        message = "Диск доступен для чтения"
+    except OSError as e:
+        message = f"Ошибка при чтении диска: {str(e)}"
+    finally:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+
+async def remount_ssd_2tb(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    try:
+        result = subprocess.run(
+            ['/bin/bash', '/home/andrey/scripts/check_mount.sh'], capture_output=True, text=True, check=True
+        )
+        message = f'SSD примонтирован {result.stdout.strip()}'
+    except subprocess.CalledProcessError as e:
+        message = f'Ошибка при примонтировании SSD: {str(e)}'
+    finally:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
 
 if __name__ == '__main__':
@@ -50,9 +76,13 @@ if __name__ == '__main__':
     start_handler = CommandHandler('start', start)
     status_handler = CommandHandler('status', status)
     help_handler = CommandHandler('help', help)
+    check_ssd_handler = CommandHandler('check_ssd_2tb', status_ssd_2tb)
+    remount_ssd_handler = CommandHandler('remount_ssd_2tb', remount_ssd_2tb)
 
     application.add_handler(start_handler)
     application.add_handler(status_handler)
     application.add_handler(help_handler)
+    application.add_handler(check_ssd_handler)
+    application.add_handler(remount_ssd_handler)
 
     application.run_polling()
